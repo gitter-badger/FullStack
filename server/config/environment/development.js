b0d6ebe568dc88c6ac48800a5d0bd57b5d0330d4
Zama Mohammed Khan <mohammedzamakhan@gmail.com>
2015-07-31 21:19:22 +0000

    Initial Commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://example:example@ds047642.mongolab.com:47642/example'
+  },
+
+  seedDB: true
+};
